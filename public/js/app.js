87f739a3dd00d7e3acba6bb259a2e06e8c99779c
Mark Hester <marky360@live.ie>
2018-12-10 01:13:39 +0000

    Profile page for servers redesign.

--- public/js/app.js
+++ public/js/app.js
@@ -15810,9 +15810,8 @@ module.exports = __webpack_require__(58);
 
 /***/ }),
 /* 18 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
--- resources/views/server/show.blade.php
+++ resources/views/server/show.blade.php
@@ -4,8 +4,108 @@
 
     <?php /** @var App\Server $server */ ?>
 
+    <section id="server-profile">
+        <div class="container p-5">
+            <div class="row">
+                <div class="col-7">
+                    <div class="labels mb-4">
+                        <ul class="list-unstyled">
+                            <li>High Rate</li>
+                        </ul>
+                    </div>
+                    <h2 class="text-white font-weight-normal">{{ $server->name }}</h2>
+                    <div class="subheading mb-3">
+                        <a href="{{ $server->website }}" class="text-transparent">{{ $server->website }}</a>
+                    </div>
+                    <div class="description mb-5">
+                        <p class="text-white">{{ $server->description }}</p>
+                    </div>
+                    <div class="stats mb-4">
+                        <ul class="list-unstyled d-flex flex-row text-white font-weight-bold">
+                            <li class="mr-3"><i class="fas fa-mouse-pointer"></i> {{ $server->clicks_count }} Clicks</li>
+                            <li class="mr-3"><i class="fas fa-poll-h"></i> {{ $server->votes_count }} Votes</li>
+                            <li class="mr-3"><i class="fas fa-stethoscope"></i> Computed Score: {{ rand(1, 100) }}</li>
+                        </ul>
+                    </div>
+                    <div class="actions">
+                        <a href="" class="btn btn-trans active">View Website</a>
+                        <a href="" class="btn btn-trans">Write Review</a>
+                        <a href="" class="btn btn-trans">Vote on Server</a>
+                    </div>
+                </div>
+                <div class="col-3 offset-2 d-flex align-items-center">
+                    <div class="">
+                        <img class="rounded screenshot" src="{{ fake()->imageUrl(500,500) }}" alt="" width="100%">
+                        <div class="stars d-flex flex-row text-white text-center mt-4">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="configuration">
+        <div class="container d-flex flex-row">
+            <div class="block">
+                <div class="value">{{ $server->config->max_base_level }}</div>
+                <div class="name">Max Base Level</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->max_job_level }}</div>
+                <div class="name">Max Job Level</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->max_aspd }}</div>
+                <div class="name">ASPD Max Stat</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->max_stats }}</div>
+                <div class="name">Max Stats</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->base_exp_rate }}x</div>
+                <div class="name">Base Exp Rate</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->job_exp_rate }}x</div>
+                <div class="name">Job Exp Rate</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->instant_cast_stat }}x</div>
+                <div class="name">Instant Cast Rate</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->drop_base_rate }}x</div>
+                <div class="name">Drop Base</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->drop_base_mvp_rate }}x</div>
+                <div class="name">Drop Base MVP</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->drop_base_special_rate }}x</div>
+                <div class="name">Drop Special Base</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->drop_base_rate }}x</div>
+                <div class="name">Drop Card Base</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->drop_base_mvp_rate }}x</div>
+                <div class="name">Drop Card MVP</div>
+            </div>
+            <div class="block">
+                <div class="value">{{ $server->config->drop_base_special_rate }}x</div>
+                <div class="name">Drop Special Card</div>
+            </div>
+        </div>
+    </section>
+
     <div class="server container mt-5">
-        
         <div class="row">
             <div class="col-9">
                 <h2 class="text-dark">{{ $server->name }}</h2>
@@ -13,7 +113,7 @@
                     <a href="{{ url($server->website) }}">{{ $server->website }}</a>
                 </div>
                 <div class="description">
-                    <p>{{ $server->description }}</p>
+                    <p>{{ fake()->sentence(300) }}</p>
                 </div>
             </div>
             <div class="col-3 d-flex flex-column justify-content-end">
@@ -23,110 +123,6 @@
             </div>
         </div>
     </div>
-
-    <section class="mt-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-7 d-flex flex-column">
-                    <div class="flex-fill">
-                        <h3 class="text-dark">Reviews</h3>
-                        <div class="row">
-                            <div class="review border rounded p-2 mr-2 mb-2" style="width: 18rem">
-                                <h5>Review #{{ rand(1, 50) }}</h5>
-                                <p>{{ fake()->paragraph }}</p>
-                            </div>
-                            <div class="review border rounded p-2 mr-2 mb-2" style="width: 18rem">
-                                <h5>Review #{{ rand(1, 50) }}</h5>
-                                <p>{{ fake()->paragraph }}</p>
-                            </div>
-                            <div class="review border rounded p-2 mr-2 mb-2" style="width: 18rem">
-                                <h5>Review #{{ rand(1, 50) }}</h5>
-                                <p>{{ fake()->paragraph }}</p>
-                            </div>
-                            <div class="review border rounded p-2 mr-2 mb-2" style="width: 18rem">
-                                <h5>Review #{{ rand(1, 50) }}</h5>
-                                <p>{{ fake()->paragraph }}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="bg-light screenshots p-3">
-                        <h3 class="text-dark">Screenshots (Flickr Slider)</h3>
-                        <div class="d-flex">
-                            <img class="screenshot mr-2" src="https://www.anomalyro.com/resources/screenshots/1.jpg" alt="" height="90" width="120">
-                            <img class="screenshot mr-2" src="https://www.anomalyro.com/resources/screenshots/2.jpg" alt="" height="90" width="120">
-                            <img class="screenshot mr-2" src="https://www.anomalyro.com/resources/screenshots/3.jpg" alt="" height="90" width="120">
-                            <img class="screenshot mr-2" src="https://www.anomalyro.com/resources/screenshots/4.jpg" alt="" height="90" width="120">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-5">
-                    <h3 class="text-dark">Server Configuration</h3>
-                    <table class="table table-hover">
-                        <thead>
-                        <tr class="bg-light">
-                            <th scope="col">Config</th>
-                            <th scope="col">Value</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row">Max Base Level</th>
-                            <td>{{ $server->config->max_base_level }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Max Job Level</th>
-                            <td>{{ $server->config->max_job_level }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Max ASPD</th>
-                            <td>{{ $server->config->max_aspd }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Max Stat Level</th>
-                            <td>{{ $server->config->max_stats }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Base Exp Rate</th>
-                            <td>{{ $server->config->base_exp_rate }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Base Job Rate</th>
-                            <td>{{ $server->config->job_exp_rate }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Instance Cast Stat</th>
-                            <td>{{ $server->config->instant_cast_stat }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><img src="http://file5.ratemyserver.net/items/small/512.gif" alt="" height="25" width="25">Drop Base Rate</th>
-                            <td>{{ $server->config->drop_base_rate }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><img src="http://file5.ratemyserver.net/items/small/512.gif" alt="" height="25" width="25">Drop Rate MVP Base</th>
-                            <td>{{ $server->config->drop_base_mvp_rate }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><img src="http://file5.ratemyserver.net/items/small/512.gif" alt="" height="25" width="25">Drop Base Special Rate</th>
-                            <td>{{ $server->config->drop_base_special_rate }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><img src="http://file5.ratemyserver.net/items/small/card.gif" alt="" height="25" width="25">Drop Card Rate</th>
-                            <td>{{ $server->config->drop_card_rate }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><img src="http://file5.ratemyserver.net/items/small/card.gif" alt="" height="25" width="25">Drop Card MVP Rate</th>
-                            <td>{{ $server->config->drop_card_mvp_rate }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><img src="http://file5.ratemyserver.net/items/small/card.gif" alt="" height="25" width="25">Drop Card Special Rate</th>
-                            <td>{{ $server->config->drop_card_special_rate }}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </section>
 
     <section class="mt-4">
         <div class="container bg-light py-3 mb-3 rounded">
@@ -223,6 +219,110 @@
                             <h4>Events</h4>
                             <p class="mb-0">Events are made reasonably by staff and are of quality and rewards.</p>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mt-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h3 class="text-dark">Server Configuration</h3>
+                    <table class="table table-hover">
+                        <thead>
+                        <tr class="bg-light">
+                            <th scope="col">Config</th>
+                            <th scope="col">Value</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Max Base Level</th>
+                            <td>{{ $server->config->max_base_level }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Max Job Level</th>
+                            <td>{{ $server->config->max_job_level }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Max ASPD</th>
+                            <td>{{ $server->config->max_aspd }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Max Stat Level</th>
+                            <td>{{ $server->config->max_stats }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Base Exp Rate</th>
+                            <td>{{ $server->config->base_exp_rate }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Base Job Rate</th>
+                            <td>{{ $server->config->job_exp_rate }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Instance Cast Stat</th>
+                            <td>{{ $server->config->instant_cast_stat }}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-6">
+                    <h3 class="text-dark">Drop Rates & EXP</h3>
+                    <table class="table table-hover">
+                        <thead>
+                        <tr class="bg-light">
+                            <th scope="col">Config</th>
+                            <th scope="col">Value</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Drop Base Rate</th>
+                            <td>{{ $server->config->drop_base_rate }}x</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Drop Rate MVP Base</th>
+                            <td>{{ $server->config->drop_base_mvp_rate }}x</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Drop Base Special Rate</th>
+                            <td>{{ $server->config->drop_base_special_rate }}x</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Drop Card Rate</th>
+                            <td>{{ $server->config->drop_card_rate }}x</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Drop Card MVP Rate</th>
+                            <td>{{ $server->config->drop_card_mvp_rate }}x</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Drop Card Special Rate</th>
+                            <td>{{ $server->config->drop_card_special_rate }}x</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mt-4">
+        <div class="container">
+            <div class="reviews">
+                <h2>Reviews</h2>
+                <div class="review p-4 d-flex flex-row rounded">
+                    <div class="profile d-flex flex-column mr-4">
+                        <img class="avatar rounded-circle mb-2" src="{{ fake()->imageUrl(50, 50) }}" alt="Profile Avatar Image" width="50">
+                        {{--<small class="subheading level font-weight-bold text-center">Level {{ rand(1, 10) }}</small>--}}
+                    </div>
+                    <div class="content">
+                        <div class="h5"><span class="font-weight-bold text-black">{{ fake()->userName }}</span>
+                            <small>• {!! fake()->dateTimeThisMonth('now')->format('d M Y') !!}</small></div>
+                        <p>{{ fake()->sentence(200) }}</p>
                     </div>
                 </div>
             </div>
