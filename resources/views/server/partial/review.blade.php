ddd4238908ac2e39ec8ad81de2100f2c20433987
Mark Hester <marky360@live.ie>
2018-12-16 19:06:20 +0000

    changes

--- resources/views/server/partial/review.blade.php
+++ resources/views/server/partial/review.blade.php
@@ -1,51 +1,65 @@
 
-<div class="review p-4">
-    <div class="info d-flex align-items-center">
-        <span class="user mr-2" style="color: #ff194d;">{{ fake()->firstName . " " . fake()->lastName }}</span>
-        <small class="mr-2">‣</small>
-        <small>Posted 22 Hours ago</small>
-        <span class="star-rating d-flex flex-row flex-fill justify-content-end">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star-half-alt"></i>
-            <i class="far fa-star"></i>
+<div class="review px-3 py-2">
+    <div class="row">
+        <div class="avatar mr-2">
+            <span class="font-weight-bold">{{ fake()->randomElement(['A', 'B', 'C', 'D', 'E', 'F', 'G']) }}</span>
+        </div>
+        <div class="col bg-white p-3 ml-3 content">
+            <div class="info d-flex align-items-center">
+                <span class="user mr-2" style="color: #ff194d;">{{ fake()->firstName . " " . fake()->lastName }}</span>
+                <small class="mr-2">‣</small>
+                <small>Posted 22 Hours ago</small>
         </span>
-    </div>
-    <div class="description">
-        {{ fake()->sentence(120) }}
-    </div>
-    <div class="scores d-flex flex-row">
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Donation Balance : <span class="font-weight-bold">{{ $score }}</span>
+            </div>
+            <div class="description">
+                {{ fake()->sentence(120) }}
+            </div>
+            <div class="scores d-flex flex-row">
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Donation Balance : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Update Balance : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Class Balance : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Item Balance : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Support : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Content : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Hosting : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+                <?php $score = rand(1, 10) ?>
+                <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
+                    Events : <span class="font-weight-bold">{{ $score }}</span>
+                </div>
+            </div>
         </div>
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Update Balance : <span class="font-weight-bold">{{ $score }}</span>
-        </div>
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Class Balance : <span class="font-weight-bold">{{ $score }}</span>
-        </div>
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Item Balance : <span class="font-weight-bold">{{ $score }}</span>
-        </div>
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Support : <span class="font-weight-bold">{{ $score }}</span>
-        </div>
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Content : <span class="font-weight-bold">{{ $score }}</span>
-        </div>
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Hosting : <span class="font-weight-bold">{{ $score }}</span>
-        </div>
-            <?php $score = rand(1, 10) ?>
-        <div class="score score-is-{{ $score >= 7 ? "good" : ($score >= 4 ? "ok" : "bad") }}">
-            Events : <span class="font-weight-bold">{{ $score }}</span>
+        <div class="rating p-3 bg-white">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <span class="text-center">
+                3.5 out of 5
+            </span>
         </div>
     </div>
 </div>
--- resources/views/server/show.blade.php
+++ resources/views/server/show.blade.php
@@ -60,108 +60,120 @@
         @endslot
     @endcomponent
 
-    <section id="curtain">
-        <div class="container px-5">
-            <div class="row">
-                <div class="col-7">
-                    <div class="labels mb-4">
-                        <ul class="list-unstyled">
-                            <li>High Rate</li>
-                        </ul>
+    <div id="server-wrapper">
+
+        <section id="curtain">
+            <div class="container">
+                <div class="row bg-light">
+                    <div class="col-7">
+                        <div class="labels mb-4 d-flex flex-row">
+                            <ul class="list-unstyled mr-2">
+                                <li>High Rate</li>
+                            </ul>
+                            <ul class="list-unstyled mr02">
+                                <li>Other Tags</li>
+                            </ul>
+                        </div>
+                        <h2 class="text-white font-weight-normal">{{ $server->name }}</h2>
+                        <div class="subheading mb-3">
+                            <a href="{{ $server->website }}" class="text-transparent">{{ $server->website }}</a>
+                        </div>
+                        <div class="description mb-5">
+                            <p class="text-white">{{ $server->description }}</p>
+                        </div>
+                        <div class="stats mb-4">
+                            <ul class="list-unstyled d-flex flex-row text-white font-weight-bold">
+                                <li class="mr-3"><i class="fas fa-mouse-pointer"></i> {{ $server->clicks_count }} Clicks</li>
+                                <li class="mr-3"><i class="fas fa-poll-h"></i> {{ $server->votes_count }} Votes</li>
+                                {{--<li class="mr-3"><i class="fas fa-stethoscope"></i> Computed Score: {{ rand(1, 100) }}</li>--}}
+                            </ul>
+                        </div>
+                        <div class="actions">
+                            <a href="" class="btn btn-trans active">View Website</a>
+                            <a href="" class="btn btn-trans">Write Review</a>
+                            <a href="" class="btn btn-trans">Vote For Server</a>
+                        </div>
                     </div>
-                    <h2 class="text-white font-weight-normal">{{ $server->name }}</h2>
-                    <div class="subheading mb-3">
-                        <a href="{{ $server->website }}" class="text-transparent">{{ $server->website }}</a>
-                    </div>
-                    <div class="description mb-5">
-                        <p class="text-white">{{ $server->description }}</p>
-                    </div>
-                    <div class="stats mb-4">
-                        <ul class="list-unstyled d-flex flex-row text-white font-weight-bold">
-                            <li class="mr-3"><i class="fas fa-mouse-pointer"></i> {{ $server->clicks_count }} Clicks</li>
-                            <li class="mr-3"><i class="fas fa-poll-h"></i> {{ $server->votes_count }} Votes</li>
-                            <li class="mr-3"><i class="fas fa-stethoscope"></i> Computed Score: {{ rand(1, 100) }}</li>
-                        </ul>
-                    </div>
-                    <div class="actions">
-                        <a href="" class="btn btn-trans active">View Website</a>
-                        <a href="" class="btn btn-trans">Write Review</a>
-                        <a href="" class="btn btn-trans">Vote on Server</a>
-                    </div>
-                </div>
-                <div class="col-3 offset-2 d-flex align-items-center">
-                    <div class="">
-                        <img class="rounded screenshot" src="{{ url("https://www.novaragnarok.com/themes/new/img/memberLoginLogo.png") }}" alt="" width="100%">
-                        <div class="stars d-flex flex-row text-white text-center mt-5">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star-half-alt"></i>
-                            <i class="far fa-star"></i>
+                    <div class="col-3 offset-2 d-flex align-items-center">
+                        <div class="">
+                            <img class="rounded screenshot" src="{{ url("https://www.novaragnarok.com/themes/new/img/memberLoginLogo.png") }}" alt="" width="100%">
+                            <div class="stars d-flex flex-row text-white text-center mt-5">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                                <i class="far fa-star"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section id="configuration" class="bg-light">
-        <div class="container p-5">
-            <h3 class="heading mb-4 text-dark heading-underline">Server Configuration Setup</h3>
-            <div class="row">
-                <div class="col-4 d-flex flex-column">
-                    @include('server.partial.config', ['name'=>'Base Exp Rate', 'value' => $server->config->base_exp_rate])
-                    @include('server.partial.config', ['name'=>'Job Exp Rate', 'value' => $server->config->job_exp_rate])
-                    @include('server.partial.config', ['name'=>'Max Base Level', 'value' => $server->config->max_base_level])
-                    @include('server.partial.config', ['name'=>'Max Job Level', 'value' => $server->config->max_job_level])
-                </div>
-                <div class="col-4 d-flex flex-column">
-                    @include('server.partial.config', ['name'=>'Max Basic Stats', 'value' => $server->config->max_stats])
-                    @include('server.partial.config', ['name'=>'Max ASPD', 'value' => $server->config-> max_aspd])
-                    @include('server.partial.config', ['name'=>'Instant Cast Stat', 'value' => 'Dex: '.$server->config->instant_cast_stat])
-                </div>
-                <div class="col-4 d-flex flex-column">
-                    @include('server.partial.config', ['name'=>'Drop Base', 'value' => $server->config->drop_base_rate])
-                    @include('server.partial.config', ['name'=>'Drop Base MVP', 'value' => $server->config->drop_base_mvp_rate])
-                    {{--@include('server.partial.config', ['name'=>'Drop Base Special', 'value' => $server->config->drop_base_special_rate])--}}
-                    @include('server.partial.config', ['name'=>'Drop Card', 'value' => $server->config->drop_card_rate])
-                    @include('server.partial.config', ['name'=>'Drop Card MVP', 'value' => $server->config->drop_card_mvp_rate])
-                    {{--@include('server.partial.config', ['name'=>'Drop Card Special', 'value' => $server->config->drop_card_special_rate])--}}
+        <section id="configuration">
+            <div class="container p-5 bg-light">
+                <h3 class="heading mb-4 text-dark heading-underline">Server Configuration Setup</h3>
+                <div class="row">
+                    <div class="col-4 d-flex flex-column">
+                        @include('server.partial.config', ['name'=>'Base Exp Rate', 'value' => $server->config->base_exp_rate])
+                        @include('server.partial.config', ['name'=>'Job Exp Rate', 'value' => $server->config->job_exp_rate])
+                        @include('server.partial.config', ['name'=>'Max Base Level', 'value' => $server->config->max_base_level])
+                        @include('server.partial.config', ['name'=>'Max Job Level', 'value' => $server->config->max_job_level])
+                    </div>
+                    <div class="col-4 d-flex flex-column">
+                        @include('server.partial.config', ['name'=>'Max Basic Stats', 'value' => $server->config->max_stats])
+                        @include('server.partial.config', ['name'=>'Max ASPD', 'value' => $server->config-> max_aspd])
+                        @include('server.partial.config', ['name'=>'Instant Cast Stat', 'value' => 'Dex: '.$server->config->instant_cast_stat])
+                    </div>
+                    <div class="col-4 d-flex flex-column">
+                        @include('server.partial.config', ['name'=>'Drop Base', 'value' => $server->config->drop_base_rate])
+                        @include('server.partial.config', ['name'=>'Drop Base MVP', 'value' => $server->config->drop_base_mvp_rate])
+                        {{--@include('server.partial.config', ['name'=>'Drop Base Special', 'value' => $server->config->drop_base_special_rate])--}}
+                        @include('server.partial.config', ['name'=>'Drop Card', 'value' => $server->config->drop_card_rate])
+                        @include('server.partial.config', ['name'=>'Drop Card MVP', 'value' => $server->config->drop_card_mvp_rate])
+                        {{--@include('server.partial.config', ['name'=>'Drop Card Special', 'value' => $server->config->drop_card_special_rate])--}}
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section id="ratings">
-        <div class="container p-5">
-            <div class="py-3 mb-3 rounded" style="border:1px solid rgba(255, 255, 255, 0.2);">
-                <h3 class="heading mb-4 text-dark heading-underline">Balance Ratings</h3>
-                <div class="row no-gutters">
-                    @include('server.partial.rating', ['name' => 'Donation Balance', 'description' => 'Can non-donators compete with donators.'])
-                    @include('server.partial.rating', ['name' => 'Update Balance', 'description' => 'Do update increase or improve balance each time.'])
-                    @include('server.partial.rating', ['name' => 'Class Balance', 'description' => 'Are classes equally as powerful.'])
-                    @include('server.partial.rating', ['name' => 'Item Balance', 'description' => 'Do items have reasonable stats against others.'])
+        <section id="ratings">
+            <div class="container pl-5 pr-5 pb-5 bg-light">
+                <div class="py-3 mb-3 rounded" style="border:1px solid rgba(255, 255, 255, 0.2);">
+                    <h3 class="heading mb-4 text-dark heading-underline">Balance Ratings</h3>
+                    <div class="row no-gutters">
+                        @include('server.partial.rating', ['name' => 'Donation Balance', 'description' => 'Can non-donators compete with donators.'])
+                        @include('server.partial.rating', ['name' => 'Update Balance', 'description' => 'Do update increase or improve balance each time.'])
+                        @include('server.partial.rating', ['name' => 'Class Balance', 'description' => 'Are classes equally as powerful.'])
+                        @include('server.partial.rating', ['name' => 'Item Balance', 'description' => 'Do items have reasonable stats against others.'])
+                    </div>
+                </div>
+                <div class="py-3 mb-3 rounded" style="border:1px solid rgba(255, 255, 255, 0.2)">
+                    <h3 class="heading mb-4 text-dark heading-underline">Server Ratings</h3>
+                    <div class="row no-gutters">
+                        @include('server.partial.rating', ['name' => 'Support', 'description' => 'GMs are available and are happy to help.'])
+                        @include('server.partial.rating', ['name' => 'Hosting', 'description' => 'Server is hosted in a good place with no-lag or spikes.'])
+                        @include('server.partial.rating', ['name' => 'Content', 'description' => 'There is much to do in game and frequently new stuff added.'])
+                        @include('server.partial.rating', ['name' => 'Events', 'description' => 'Events have good rewards and are usual.'])
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section id="reviews">
+            <div class="container pl-5 pr-5 pb-5 bg-light">
+                <h3 class="heading mb-4 text-dark heading-underline">Reviews</h3>
+                <div class="reviews">
+                    @include('server.partial.review')
+                    @include('server.partial.review')
+                    @include('server.partial.review')
+                    @include('server.partial.review')
+                    @include('server.partial.review')
                 </div>
             </div>
-            <div class="py-3 mb-3 rounded" style="border:1px solid rgba(255, 255, 255, 0.2)">
-                <h3 class="heading mb-4 text-dark heading-underline">Server Ratings</h3>
-                <div class="row no-gutters">
-                    @include('server.partial.rating', ['name' => 'Support', 'description' => 'GMs are available and are happy to help.'])
-                    @include('server.partial.rating', ['name' => 'Hosting', 'description' => 'Server is hosted in a good place with no-lag or spikes.'])
-                    @include('server.partial.rating', ['name' => 'Content', 'description' => 'There is much to do in game and frequently new stuff added.'])
-                    @include('server.partial.rating', ['name' => 'Events', 'description' => 'Events have good rewards and are usual.'])
-                </div>
-            </div>
+        </section>
 
-        </div>
-    </section>
-
-    <section id="reviews" class="mt-4">
-        <div class="container p-5">
-            <div class="reviews">
-                <h2 class="text-dark heading heading-underline">Reviews</h2>
-                @include('server.partial.review')
-            </div>
-        </div>
-    </section>
+    </div>
 
 @endsection
