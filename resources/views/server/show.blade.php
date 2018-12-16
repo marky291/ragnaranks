6ddec7727b6ab3310eec8ab9fee5a91199ddb61a
Mark Hester <marky360@live.ie>
2018-12-16 22:04:18 +0000

    changes

--- resources/views/server/show.blade.php
+++ resources/views/server/show.blade.php
@@ -4,67 +4,11 @@
 
     <?php /** @var App\Server $server */ ?>
 
-    @component('slots.spotlight')
-        @slot('content')
-            <div class="row">
-                <div class="col-10 server-rates">
-                    <div id="filter" class="d-flex py-4">
-                        <h3 class="d-flex align-items-center mb-0 mr-4">I'm Looking for:</h3>
-
-                        <select class="form-control-sm mr-2">
-                            <option value="all">Any Rates</option>
-                            <option value="">Official Rates</option>
-                            <option value="">Low Rates</option>
-                            <option value="">Mid Rates</option>
-                            <option value="">High Rates</option>
-                            <option value="">Super Rates</option>
-                            <option value="">Instant Rates</option>
-                        </select>
-
-                        <select class="form-control-sm mr-2">
-                            <option value="all">Any Mode</option>
-                            @foreach(\App\ServerMode::all() as $mode)
-                                <option value="">{{ ucfirst($mode->name) }} Mode</option>
-                            @endforeach
-                        </select>
-
-                        <select class="form-control-sm mr-2">
-                            <option value="all">With Any Tags</option>
-                            @foreach(\App\Tag::all() as $tag)
-                                <option>With {{ ucfirst($tag->name) }}</option>
-                            @endforeach
-                        </select>
-
-                        <select class="form-control-sm mr-2">
-                            <option>Sorted by Score</option>
-                            <option>Sorted by Rank Position</option>
-                            <option>Sorted by Date added</option>
-                            <option>Sorted by Online since</option>
-                        </select>
-
-                        <select class="form-control-sm">
-                            <option>And show 50 servers</option>
-                            <option>And show 100 servers</option>
-                            <option>And show 250 servers</option>
-                            <option>And show 500 servers</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="col-2 justify-content-end align-self-center text-right">
-                    Search Servers
-                    <a class="text-muted" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
-                    </a>
-                </div>
-            </div>
-        @endslot
-    @endcomponent
-
     <div id="server-wrapper">
 
-        <section id="curtain">
+        <section id="curtain" class="bg-light">
             <div class="container">
-                <div class="row bg-light">
+                <div class="row">
                     <div class="col-7">
                         <div class="labels mb-4 d-flex flex-row">
                             <ul class="list-unstyled mr-2">
@@ -110,8 +54,8 @@
             </div>
         </section>
 
-        <section id="configuration">
-            <div class="container p-5 bg-light">
+        <section id="configuration" class="bg-light">
+            <div class="container p-5">
                 <h3 class="heading mb-4 text-dark heading-underline">Server Configuration Setup</h3>
                 <div class="row">
                     <div class="col-4 d-flex flex-column">
@@ -137,8 +81,8 @@
             </div>
         </section>
 
-        <section id="ratings">
-            <div class="container pl-5 pr-5 pb-5 bg-light">
+        <section id="ratings" class="bg-light">
+            <div class="container pl-5 pr-5 pb-5">
                 <div class="py-3 mb-3 rounded" style="border:1px solid rgba(255, 255, 255, 0.2);">
                     <h3 class="heading mb-4 text-dark heading-underline">Balance Ratings</h3>
                     <div class="row no-gutters">
@@ -161,8 +105,8 @@
             </div>
         </section>
 
-        <section id="reviews">
-            <div class="container pl-5 pr-5 pb-5 bg-light">
+        <section id="reviews" class="bg-light">
+            <div class="container pl-5 pr-5 pb-5">
                 <h3 class="heading mb-4 text-dark heading-underline">Reviews</h3>
                 <div class="reviews">
                     @include('server.partial.review')
