7292ce9a27ed778829d4b283858bca12831d8f39
Mark Hester <Marky360@live.ie>
2019-01-19 00:14:11 +0000

    Configuration redesign (#2)
    * Redesign V1
    
    * Carousel
    
    * Scoreboard design.

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,12 @@ window.Vue = require('vue');
 
 window.Event = new Vue();
 
+// Carousel Element.
+import Vue from 'vue';
+import VueCarousel from 'vue-carousel';
+Vue.use(VueCarousel);
+// =-=-=-=-=-=-=-=-=-=-=-=
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/components/ScoreboardComponent.vue
+++ resources/js/components/ScoreboardComponent.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="col mr-2 d-flex align-items-center rounded overflow-hidden" :class="ScoreScale(this.score)">
-        <div class="rating-block d-flex flex-row p-3 h-100">
-            <div class="d-flex">
+
+    <div class="col d-flex align-items-center rounded overflow-hidden d-flex flex-column">
+        <div class="rating-block d-flex flex-row py-3 h-100">
+            <div class="d-flex align-items-center">
                 <div class="w-75">
-                    <h4 class="text-light">{{ this.title }}</h4>
-                    <p class="text-light mb-0">{{ this.description }}</p>
+                    <h4 class="text-dark">{{ this.title }}</h4>
+                    <p class="text-dark mb-0">{{ this.description }}</p>
                 </div>
-                <span class="score position-absolute text-transparent h-100">
-                    {{ this.score }}
+                <span class="score position-absolute text-transparent h-100 d-flex align-items-center">
+                    <i class="fas fa-medal" :class="ScoreScale(this.score)"></i>
                 </span>
             </div>
+        </div>
+        <div class="w-100 bg-light py-1 px-2">
+            <small><b>Score</b>: {{ score }} / 10</small>
         </div>
     </div>
 </template>
