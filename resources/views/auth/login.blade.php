442ac4791acaf03e8ba1e1e8a168c0b1b16eadf6
Mark Hester <marky360@live.ie>
2019-05-18 10:48:53 +0100

    Initial layout of the login form #15

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,35 @@
-@extends('layouts.app')
+@extends('layouts.frame')
 
 @section('content')
+
+    <div class="shadow-inner">
+        <div class="container">
+            <div class="row">
+                <div class="w-50 m-auto mt-5 p-2 tw-rounded tw-items-center tw-flex" style="height: 75vh;">
+                    <div class="mb-4 tw-flex-1 tw-bg-white px-5 py-4 tw-rounded shadow">
+                        <div class="tw-flex tw-flex-row tw-items-center mb-3">
+                            <img src="https://pngimage.net/wp-content/uploads/2018/06/poring-png-4.png" alt="poring monster" width="75">
+                            <h2 class="ml-2 tw-font-bold mb-0">Be Part of the Community, Login! <br><small>Don't have an account? <span><a href="">Register now!</a></span></small></h2>
+                        </div>
+                        <div class="mb-3">
+                            <p class="tw-font-bold mb-1">Enter your Email Account</p>
+                            <at-input v-model="inputValue" placeholder="Email"></at-input>
+                        </div>
+                        <div class="mb-4">
+                            <p class="tw-font-bold mb-1">Enter your password</p>
+                            <at-input v-model="inputValue" type="password" placeholder="Password"></at-input>
+                        </div>
+                        <at-checkbox v-model="checkboxValue1" style="display:inherit" class="mb-4" label="Remember">Remember me</at-checkbox>
+                        <at-button type="primary">Login</at-button>
+                        <at-button type="text">Forgot Yout Password?</at-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
