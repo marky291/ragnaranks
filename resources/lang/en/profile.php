13f4bcf3e1bc3f9286beda2265a90f11d7f0a336
MitsuneBewbs <50927576+MitsuneBewbs@users.noreply.github.com>
2019-05-26 19:02:24 -0600

    Update profile.php
    Simplifying. People don't read anyways GG

--- resources/lang/en/profile.php
+++ resources/lang/en/profile.php
@@ -4,7 +4,7 @@ return [
 
     'creator' => [
         'default_description' => '# Welcome to RagnaRanks markdown editor!
-You can write something nice and descriptive with a huge amount of different formats! [Markdown Guide](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+You can write something nice and descriptive with a huge amount of different formats! [Guide](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
 You can also use emojis copied from the web and paste them right here! 😍
 
 **Please utilize all the configuration options to allow us to maximize the potential of your listing(s)!**',
