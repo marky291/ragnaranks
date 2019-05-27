fa7494801792d79f9c6df5db193eeb0e09ec2df7
MitsuneBewbs <50927576+MitsuneBewbs@users.noreply.github.com>
2019-05-26 18:58:41 -0600

    Update profile.php
    Oops, put markdown link in wrong spot-- my bad. Fixed!

--- resources/lang/en/profile.php
+++ resources/lang/en/profile.php
@@ -3,8 +3,8 @@
 return [
 
     'creator' => [
-        'default_description' => '# Welcome to RagnaRanks markdown editor! [Markdown Guide](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
-You can write something nice and descriptive with a huge amount of different syntax formats!
+        'default_description' => '# Welcome to RagnaRanks markdown editor!
+You can write something nice and descriptive with a huge amount of different syntax formats! [Markdown Guide](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
 You can also use emojis copied from the web and paste them right here! 😍
 
 **Please utilize all the configuration options to allow us to maximize the potential of your listing(s)!**',
