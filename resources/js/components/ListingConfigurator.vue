04fb4031e629d0f2c61049512e07d0aabe0c652d
MitsuneBewbs <50927576+MitsuneBewbs@users.noreply.github.com>
2019-05-26 21:09:32 -0600

    Update ListingConfigurator.vue
    Added aqua, black, mauve, pink links to generatePreset. All respective images added.

--- resources/js/components/ListingConfigurator.vue
+++ resources/js/components/ListingConfigurator.vue
@@ -38,6 +38,10 @@
 										{ accent: 'purple', background: '/img/preset/card-purple.png'},
 										{ accent: 'green', background: '/img/preset/card-green.png'},
 										{ accent: 'blue', background: '/img/preset/card-blue.png'},
+                                        { accent: 'aqua', background: '/img/preset/card-aqua.png'},
+                                        { accent: 'black', background: '/img/preset/card-black.png'},
+                                        { accent: 'mauve', background: '/img/preset/card-mauve.png'},
+                                        { accent: 'pink', background: '/img/preset/card-pink.png'},                                        
 									]);
                 this.listing.accent = preset.accent;
                 this.listing.background = preset.background;
