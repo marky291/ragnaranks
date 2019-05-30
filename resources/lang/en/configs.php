9c604a6886b8545dcc6d10a46a72f62711ef803a
MitsuneBewbs <50927576+MitsuneBewbs@users.noreply.github.com>
2019-05-30 16:58:13 -0600

    Update configs.php
    Wording and grammar fixed.

--- resources/lang/en/configs.php
+++ resources/lang/en/configs.php
@@ -3,19 +3,19 @@
 return [
     'max_base_level' => [
         'name' => 'Max Base',
-        'describe' => 'The highest base level that can be achieved',
+        'describe' => 'The highest base level that can be achieved.',
     ],
     'max_job_level' => [
         'name' => 'Max Job',
-        'describe' => 'The highest job level that can be achieved',
+        'describe' => 'The highest job level that can be achieved.',
     ],
     'max_aspd' => [
         'name' => 'Max ASPD',
-        'describe' => 'Maximum atk speed. (Default 190, Highest allowed 199)'
+        'describe' => 'Maximum attack speed. (Default 190, Highest allowed 199)'
     ],
     'max_stats' => [
         'name' => 'Max Stats',
-        'describe' => 'The maximum stat parameter that can be selected'
+        'describe' => 'The maximum stat parameter that can be selected.'
     ],
     'arrow_decrement' => [
         'name' => 'Unlimited Arrows',
@@ -23,71 +23,71 @@ return [
     ],
     'undead_detect_type' => [
         'name' => 'Detect Undead',
-        'describe' => 'Should race or element be used to consider someone undead?',
+        'describe' => 'Does race or element consider someone undead?',
     ],
     'attribute_recover' => [
         'name' => 'Attribute Recovery',
-        'describe' => 'Does HP recover if hit by an attribute that\'s same as your own?',
+        'describe' => 'Does HP recover if hit by an attribute that's the same as your own?',
     ],
     'cast_rate' => [
         'name' => 'Cast Rate',
-        'describe' => 'The rate of time it takes to cast a spell'
+        'describe' => 'The rate of time it takes to cast a spell. (100 = 100%)'
     ],
     'delay_rate' => [
         'name' => 'Delay Rate',
-        'describe' => 'Delay time after casting',
+        'describe' => 'Delay time after casting. (100 = 100%)',
     ],
     'castrate_dex_scale' => [
         'name' => 'Dex Required',
-        'describe' => 'At what dex does the cast time become zero (instacast)',
+        'describe' => 'Amount of DEX required before cast time is zero. (IE: Instant Cast)',
     ],
     'vcast_stat_scale' => [
         'name' => 'vcast_stat_scale',
-        'describe' => 'How much (dex*2+int) does variable cast turns zero',
+        'describe' => 'Amount of (DEX*2+INT) before variable cast turn zero.',
     ],
     'base_exp_rate' => [
         'name' => 'Base EXP',
-        'describe' => 'Rate at which base exp. is given.',
+        'describe' => 'Rate at which base experience is given.',
     ],
     'job_exp_rate' => [
         'name' => 'Job EXP',
-        'describe' => 'Rate at which job exp. is given.',
+        'describe' => 'Rate at which job experience is given.',
     ],
     'quest_exp_rate' => [
         'name' => 'Quest EXP',
-        'describe' => 'Rate of base/job exp given by NPCs.',
+        'describe' => 'Rate of base/job experience given by NPCs.',
     ],
 
     'item_drop_common' => [
         'name' => 'Common Drops',
-        'describe' => 'The rate the common items are dropped (Items that are in the ETC tab, besides card)',
+        'describe' => 'The rate at which common ETC items are dropped. (Cards excluded)',
     ],
     'item_drop_equip' => [
         'name' => 'Equip Drops',
-        'describe' => 'The rate at which equipment is dropped.',
+        'describe' => 'The rate at which equipment are dropped.',
     ],
     'item_drop_card' => [
         'name' => 'Card Drops',
-        'describe' => 'The rate at which cards are dropped',
+        'describe' => 'The rate at which cards are dropped.',
     ],
     'item_drop_treasure' => [
         'name' => 'Treasure Drops',
-        'describe' => 'Rate adjustment for Treasure Box drops',
+        'describe' => 'Rate adjustment for Treasure Box drops.',
     ],
     'item_drop_common_mvp' => [
         'name' => 'MVP Common Drops',
-        'describe' => 'The rate the common items are dropped by MVPS (Items that are in the ETC tab, besides card)',
+        'describe' => 'The rate at which common ETC items are dropped by MVPs. (Cards excluded)',
     ],
     'item_drop_equip_mvp' => [
         'name' => 'MVP Equip Drops',
-        'describe' => 'The rate at which equipment is dropped by MVPS.',
+        'describe' => 'The rate at which equipment are dropped by MVPs.',
     ],
     'item_drop_card_mvp' => [
         'name' => 'MVP Card Drops',
-        'describe' => 'The rate at which cards are dropped',
+        'describe' => 'The rate at which cards are dropped by MVPs.',
     ],
     'drops_by_luk' => [
         'name' => 'Influenced Drops',
-        'describe' => 'Does luck affect the drops by absolute basis.',
+        'describe' => 'Does LUK stat affect the drops by absolute basis?',
     ],
 ];
