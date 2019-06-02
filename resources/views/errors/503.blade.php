9591cfd8017b095c0d852f8a53f78419a72617e1
Mark Hester <marky360@live.ie>
2019-06-02 10:44:31 +0100

    Maintenance mode test

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
 @section('title', __('Service Unavailable'))
 
 @section('image')
-    <div style="background-image: url({{ asset('/svg/503.png') }}); background-size:contain;" class="absolute pin bg-no-repeat md:bg-left lg:bg-center"></div>
+    <div style="background-image: url({{ asset('/svg/503.png') }}); background-size:contain; background-position:center;" class="absolute pin bg-no-repeat md:bg-left lg:bg-center"></div>
 @endsection
 
 @section('message', __($exception->getMessage() ?: 'We are busy creating something amazing, check back soon!'))
