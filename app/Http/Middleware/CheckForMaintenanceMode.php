931b31df3e3e2ed2fc48d22c0dbb91e0d2ffc320
Mark Hester <marky360@live.ie>
2019-06-02 10:40:26 +0100

    Except certain IPs maintenance mode

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\IpUtils;
 
 class CheckForMaintenanceMode extends Middleware
 {
@@ -11,7 +14,28 @@ class CheckForMaintenanceMode extends Middleware
      *
      * @var array
      */
-    protected $except = [
-        //
+    protected $except = [];
+
+    /**
+     * The IP addresses that are exempt from the maintenance mode.
+     *
+     * @var array
+     */
+    protected $exemptIP = [
+      '78.18.76.0/8'
     ];
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (IpUtils::checkIp($request->ip(), $this->exemptIP)) {
+            return $next($request);
+        }
+
+        return parent::handle($request, $next);
+    }
 }
