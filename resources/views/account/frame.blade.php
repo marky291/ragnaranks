4d31d3d7d0e6e132f78b9e8fad72dfcd328e5d28
Mark Hester <marky360@live.ie>
2019-06-04 12:29:17 +0100

    Corrected Url

--- resources/views/account/frame.blade.php
+++ resources/views/account/frame.blade.php
@@ -27,7 +27,7 @@
                                 <li class="tw-py-2" {{ $selected === 'servers' ? 'selected' : null }}><a class="tw-border-l-4 hover:tw-border-purple tw-px-6 hover:tw-no-underline tw-pb-1 tw-text-sm tw-text-grey-darkest" href="/account/servers">My Servers</a></li>
                                 @endrole
                                 @can('moderate')
-                                <li class="tw-py-2" {{ $selected === 'moderate/reports' ? 'selected' : null }}><a class="tw-border-l-4 hover:tw-border-purple tw-px-6 hover:tw-no-underline tw-pb-1 tw-text-sm tw-text-grey-darkest" href="/moderate/reports">Moderator Tools</a></li>
+                                <li class="tw-py-2" {{ $selected === 'moderate/report' ? 'selected' : null }}><a class="tw-border-l-4 hover:tw-border-purple tw-px-6 hover:tw-no-underline tw-pb-1 tw-text-sm tw-text-grey-darkest" href="/moderate/report">Moderator Tools</a></li>
                                 @endcan
                             </ul>
                         </div>
