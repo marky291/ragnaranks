5160731ec22533eaebfab9268b3ebb0a1fd05116
Mark Hester <Marky360@live.ie>
2019-06-07 18:35:38 +0000

    Apply fixes from StyleCI

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,11 @@
 |
 */
 
-
-use App\Http\Resources\ListingResource;
 use App\Listings\Listing;
+use App\Http\Resources\ListingResource;
 
 Route::get('/listings', function () {
-    return cache()->remember('listing', 30, function() {
+    return cache()->remember('listing', 30, function () {
         return ListingResource::collection(App\Listings\Listing::relations()->whereIn('id', app('rankings')->take(7)->keys())->paginate(7)->sortBy('rank'));
     });
 });
@@ -27,7 +26,7 @@ Route::get('/listing/tags', function () {
 });
 
 Route::get('/listing/{listing}', function (Listing $listing) {
-    return cache()->remember("listing:{$listing->name}", 30, function() use ($listing) {
+    return cache()->remember("listing:{$listing->name}", 30, function () use ($listing) {
         return App\Http\Resources\ListingResource::make($listing->load('tags'));
     });
 });
