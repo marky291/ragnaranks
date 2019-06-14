5368752ac42dabf88dcf237b9f2b2088f7b5e75c
Mark Hester <marky360@live.ie>
2019-06-14 13:00:00 +0100

    remove dependency

---
+++ resources/js/messages.js
@@ -0,0 +1,6 @@
+// messages.js
+export default {
+    // The key format should be: 'locale.filename'.
+    'en.homepage': require('../../resources/lang/en/homepage.php'),
+    'en.profile': require('../../resources/lang/en/profile.php'),
+}
