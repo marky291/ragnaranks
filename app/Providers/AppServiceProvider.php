a7d7986b90e30bf047aba3352826fc323c1fed25
Mark Hester <marky360@live.ie>
2019-06-28 22:12:49 +0100

    Specified key was too long; max key length is 767 bytes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Listings\ListingConfiguration;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Observers\ConfigurationObserver;
 use Illuminate\Http\Resources\Json\Resource;
@@ -16,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         ListingConfiguration::observe(ConfigurationObserver::class);
     }
 
