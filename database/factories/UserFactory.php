1d680be2369c4bf08caf07d0e5bd8be5a61fd804
Mark Hester <marky360@live.ie>
2019-07-25 14:23:46 +0100

    Poring avatar on account creation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ $factory->define(User::class, static function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => str_random(10),
-        'avatarUrl' => $faker->imageUrl(640, 480),
+        'avatarUrl' => 'http://ragnaranks.com/img/preset/avatar.png',
     ];
 });
 
