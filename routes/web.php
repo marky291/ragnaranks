f535e7ee0e0c278d7fad73078efb240daf9b7112
Mark Hester <marky360@live.ie>
2019-08-01 18:22:11 +0100

    remove debug sentry

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
-
 // authentication routes (login & logout)
 Auth::routes(['verify' => true]);
 
