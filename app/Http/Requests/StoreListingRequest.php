81f9dbea849ba983340252d8bc12bc66ff96a536
Mark Hester <marky360@live.ie>
2019-08-01 12:30:04 +0100

    travis protection

--- app/Http/Requests/StoreListingRequest.php
+++ app/Http/Requests/StoreListingRequest.php
@@ -33,7 +33,7 @@ class StoreListingRequest extends FormRequest
             'accent' => Rule::in(config('filter.accents')),
             'language' => Rule::in(config('filter.languages')),
             'background' => 'active_url|required|min:3',
-            'website' => 'required|min:3|active_url',
+            'website' => 'active_url|required|min:3',
             'mode' => Rule::in(config('filter.modes')),
             'tags.*' => Rule::in(config('filter.tags')),
             'screenshots.*' => 'active_url|string',
