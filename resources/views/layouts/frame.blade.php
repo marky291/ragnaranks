6856945ccbe99e066221809a00b11a8b60c3a64d
Mark Hester <marky360@live.ie>
2019-08-06 22:28:25 +0100

    viewport

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 {{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
-        <meta name="viewport" content="width=1200, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial -scale=1">
 
         <title>RagnaRanks.com</title>
 
