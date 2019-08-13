6cb45b4f2d104cb4eef4c261eaa9b265a7f2d241
Mark Hester <marky360@live.ie>
2019-08-13 01:39:52 +0100

    update version

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     | and that deployments are loading the correct filesource.
     |
     */
-    'version' => '1.1.15',
+    'version' => '1.1.16',
 
     /*
     |--------------------------------------------------------------------------
