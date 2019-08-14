3d782451fcdfff49a9a1443d60497d6c1ee7d461
Mark Hester <marky360@live.ie>
2019-08-14 10:57:16 +0100

    remove mobile

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -7,7 +7,7 @@
 
         <meta name="robots" content="all">
         <meta name="viewport" content="width=1200"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="Description" content="@yield('description', 'Browse hundreds of ragnarok online private server listings, with advanced filtering, reviews & voting we make it a breeze.')">
 
