81672f1b9acb9677ee849bbadedf912668bf077c
Mark Hester <marky360@live.ie>
2019-08-28 22:22:35 +0100

    Message should show when logged in.

--- resources/views/listing/index.blade.php
+++ resources/views/listing/index.blade.php
@@ -8,18 +8,20 @@
     <homepage inline-template>
         <div class="shadow-inner">
             <div class="tw-container tw-pt-5" id="listingView">
-                @if (auth()->check() && !auth()->user()->hasVerifiedEmail())
-                <div class="row">
-                    <div class="col-12 pb-3">
-                        <at-alert message="Verification Required" description="A verification email has been sent and awaiting response, account functionality is limited until completed, email may be found in spam or inbox folder!" type="error" show-icon></at-alert>
+                @if (auth()->check())
+                    <div class="row">
+                        <div class="col-12 pb-3">
+                            <at-alert show-icon message="We are currently investigating issues with some images failing to upload and hope to have this fixed shortly." type="error"></at-alert>
+                        </div>
                     </div>
-                </div>
+                    @if (!auth()->user()->hasVerifiedEmail())
+                        <div class="row">
+                            <div class="col-12 pb-3">
+                                <at-alert message="Verification Required" description="A verification email has been sent and awaiting response, account functionality is limited until completed, email may be found in spam or inbox folder!" type="error" show-icon></at-alert>
+                            </div>
+                        </div>
+                    @endif
                 @endif
-                <div class="row">
-                    <div class="col-12 pb-3">
-                        <at-alert show-icon message="We are currently investigating issues with some images failing to upload and hope to have this fixed shortly." type="error"></at-alert>
-                    </div>
-                </div>
                 <div class="tw-flex pb-2 pt-2 tw-flex-wrap">
                     <div id="sidebar" class="lg:tw-w-1/3 tw-px-3 tw-flex-1">
                         @include('sidebar.message')
