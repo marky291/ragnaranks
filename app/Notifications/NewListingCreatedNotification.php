8158d687e913d6b5cf0d4e5a130ef16423b6a56d
Mark Hester <marky360@live.ie>
2019-09-05 19:32:44 +0100

    Error: Class 'App\Notifications\NewListingCreatedNotification' not found

---
+++ app/Notifications/NewListingCreatedNotification.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Listings\Listing;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NewListingCreatedNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+    /**
+     * @var Listing
+     */
+    private $listing;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Listing $listing
+     */
+    public function __construct(Listing $listing)
+    {
+        $this->listing = $listing;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('New Listing Posted!')
+            ->line('Good News, as an admin you get this unique email letting you know that a new listing has posted!')
+            ->line("Listing: {$this->listing->name}, Created: {$this->listing->created_at}.");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
