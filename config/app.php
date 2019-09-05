128db989f7d5ac999d4e3398d05c37ee5c70d2d5
Mark Hester <marky360@live.ie>
2019-09-05 14:42:23 +0100

    Version 1.4.0

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     | and that deployments are loading the correct filesource.
     |
     */
-    'version' => '1.3.4',
+    'version' => '1.4.0',
 
     /*
     |--------------------------------------------------------------------------
