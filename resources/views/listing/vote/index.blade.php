43c786cb2fc4ee9021d1980fe29b949112affc4f
Mark Hester <Marky360@live.ie>
2019-10-23 19:41:39 +0100

    Update index.blade.php

--- resources/views/listing/vote/index.blade.php
+++ resources/views/listing/vote/index.blade.php
@@ -16,21 +16,7 @@
                         :inline="false"
                         :action="'vote'">
                     </google-re-captcha-v3>
-
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- Test -->
-                    <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-2280364578236676"
-                     data-ad-slot="4462624464"
-                     data-ad-format="auto"
-                     data-full-width-responsive="true"></ins>
-                    <script>
-                    window.onload = function() {
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    }
-                    </script>
-
+                    
                     <at-button @click="sendVote()" type="primary" class="mt-2 tw-h-10 tw-w-full">Vote for @{{ listingName }}</at-button>
                 </div>
             </div>
