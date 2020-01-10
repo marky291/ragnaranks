d625cb7df1c7ffde587676965372d5f26e4217e9
Mark Hester <marky360@live.ie>
2020-01-10 16:39:56 +0000

    Update site messages for the website

--- resources/views/sidebar/message.blade.php
+++ resources/views/sidebar/message.blade.php
@@ -1,6 +1,11 @@
 <div class="content tw-shadow tw-hidden lg:tw-block tw-p-6">
-	<h3 class="text-orange font-weight-bold">Site Messages</h3>
-	<p class="subheading">We are always interested in listening to feedback and improving our
-		service, let your voice be heard in our subreddit <a href="https://www.reddit.com/r/RagnaRanks">r/Ragnaranks</a> or <a href="http://discord.gg/WGSAce2">Discord</a>!
+	<h3 class="text-orange font-weight-bold">Site Notice</h3>
+	<p class="subheading">We are keen to learn how we can improve our service,
+        let us know your opinions and ideas and get in touch with us on our <a href="https://www.m.me/ragnaranks"><i class="fab fa-facebook-square"></i> Facebook Page</a>!
 	</p>
 </div>
+
+{{--<div class="content mt-2 tw-shadow tw-hidden lg:tw-block tw-p-5">--}}
+{{--    <p class="mt-0 font-weight-bold" style="color: #3b688c">Understanding votes & clicks</p>--}}
+{{--    <p class="subheading">We store data on votes and clicks for {{ config('ranking.ignore_after_days') }} days, once this has surpassed we remove it from our site.</p>--}}
+{{--</div>--}}
