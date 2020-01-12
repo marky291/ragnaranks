70bcbaa04fede580a328c113fe1c9cbffe0b0f0a
Mark Hester <marky360@live.ie>
2020-01-12 15:51:03 +0000

    Footer displays 2019, should be 2020

--- resources/views/sidebar/display/coins.blade.php
+++ resources/views/sidebar/display/coins.blade.php
@@ -2,6 +2,6 @@
     <div class="d-flex align-items-center">
         <img class="mr-1" src="/img/icons/coin.png" alt="" height="20" width="20">
         <p >Welcome <span class="font-weight-bold">{{ user()->username  }}</span>, you have <span class="font-weight-bold">{{ user()->coins }}</span> coins to
-            <a href="https://ragnaranks.github.io/docs/earning-coins.html">spend</a>!</p>
+            <a href="https://ragnaranks.github.io/docs/earning-coins.html">spend</a></p>
     </div>
 </div>
