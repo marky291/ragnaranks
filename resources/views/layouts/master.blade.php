3f872b1139c1c2e5c07de77fc252cb8eecdd9a95
Mark Hester <marky360@live.ie>
2020-01-31 20:49:44 +0000

    Set viewport to 1200 width, v2.6.4

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link href="@yield('canonical', '')" rel="canonical">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-        <meta name="viewport" content="width=device-width, initial -scale=1">
+        <meta name="viewport" content="width=1200"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"></head>
     <body>
         <div id="app" class="min-width:1200px">
