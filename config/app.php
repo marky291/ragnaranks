7a3ff1c14c854f98d2ad6bccbc5e1ce68a67a7a7
Mark Hester <marky360@live.ie>
2020-01-31 18:39:42 +0000

    Version 2.6.0

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     | and that deployments are loading the correct filesource.
     |
     */
-    'version' => '2.5.2',
+    'version' => '2.6.0',
 
     /*
     |--------------------------------------------------------------------------
